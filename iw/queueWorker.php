51e5ae3a27b229d508b2d86d295143c2ca762a71
rkononov <rkononov@gmail.com>
2012-03-05 10:54:46 +0600

    added phpfog compatibility

--- iw/queueWorker.php
+++ iw/queueWorker.php
@@ -2,11 +2,16 @@
 include(__DIR__.'/../lib/IronWorkerWrapper.php');
 $config = parse_ini_file(__DIR__.'/../config.ini', true);
 $url = $_REQUEST['url'];
-$tmpdir = $_SERVER['TMP_DIR'];
 $name = "imageWorker.php";
+$tmpdir = $_SERVER['TMP_DIR'];
+if (empty($tmpdir)){
+$tmpdir = dirname(__FILE__);
+}
+print_r($tmpdir);
 $zipName = $tmpdir."/$name.zip";
 $file = IronWorker::zipDirectory(dirname(__FILE__)."/../workers", $zipName, true);
 $res = $iw->postCode('imageWorker.php', $zipName, $name);
+print_r($res);
 $payload = array(
     's3' => array(
         'access_key' => $config['s3']['access_key'],
