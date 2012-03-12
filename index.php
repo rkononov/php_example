66572d7c8076854bc1fc31cb1a2cb9f83fc48de3
rkononov <rkononov@gmail.com>
2012-03-12 21:15:15 +0600

    updated worker (new transformations), changed UI

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <script type="text/javascript"
         src="https://www.google.com/jsapi?key=ABQIAAAAhes0f80sBcwL-h5xCNkkgxQBmiBpQeSpIciQPfZ5Ss-a60KXIRQOVvqzsNpqzhmG9tjky_5rOuaeow"></script>
@@ -8,46 +8,73 @@ google.load('jqueryui', '1'); </script>
 <head></head>
 <body>
 <table class="sample">
-<tr>
-<td VALIGN="top">
-<h3>Add picture via posting url to IronMQ</h3>
-<br/>
-$ironmq = new IronMQ('config.ini');<br/>
-$ironmq->postMessage("input_queue", array("body" => $url_to_picture));<br/>
-<h3>Or add url to pic in form below</h3>
-<form action="/mq/postMessage.php" id="sendMessageForm">
-    <input id = "pic_url" type="text" name="url" placeholder="Search..."/>
-    <input type="submit" value="Add picture"/>
-</form>
-<div id="result"></div>
+    <tr>
+        <td VALIGN="top">
+            <h3>Add picture via posting url to IronMQ</h3>
+            <br/>
+            $ironmq = new IronMQ('config.ini');<br/>
+            $ironmq->postMessage("input_queue", array("body" => $url_to_picture));<br/>
 
-</td>
-<td VALIGN="top">
-<h3>List of pictures pushed to workers</h3>
-<div id="input_queue"></div>
-</td>
-<td VALIGN="top">
-<h3>List of processed pictures</h3>
-<div id="output_queue"></div>
-</td>
+            <h3>Or add url to pic in form below</h3>
+
+            <form action="/mq/postMessage.php" id="sendMessageForm">
+                <input id="pic_url" type="text" name="url" placeholder="Search..."/>
+                <input type="submit" value="Add picture"/>
+            </form>
+            <div id="result"></div>
+
+        </td>
+        <td VALIGN="top">
+            <table id="output">
+                <tbody>
+                <tr>
+                    <td>
+                        Image from queue
+                    </td>
+                    <td>
+                        Thumbnail
+                    </td>
+                    <td>
+                        Rotated
+                    </td>
+                    <td>
+                        Grayscale
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
 </table>
 
 <script>
-    function queue_worker(data)
-    {
-        $.post('/iw/queueWorker.php', { url:data });
+    function queue_worker(data) {
+        var task_id = ''
+        $.ajaxSetup({async:false});
+        $.post('/iw/queueWorker.php', { url:data },function(data) {
+            task_id = data;
+        });
+        return task_id;
     }
     $(document).ready(function () {
         setInterval(function () {
             $.get('/mq/getMessage.php?queue_name=input_queue', null, function (data) {
                 if (data) {
-                    $("#input_queue").append(data + '<br/>');
-                    queue_worker(data);
+                    var task_id = queue_worker(data);
+                    var image = '<img width="200" src="' + data + '"/><br/>';
+                    var txt = '<td><span id="'+ task_id + 'thumb"><img width="200" src="ajax-loader.gif"/><br/></span></td><td><span id="';
+                    var html = '<tr><td>' + image + '</td><td><span id="' +
+                        task_id + '_thumb"><img width="200" src="ajax-loader.gif"/><br/></span></td><td><span id="' +
+                        task_id + '_rotated"><img width="200" src="ajax-loader.gif"/><br/></span></td><td><span id="' +
+                        task_id + '_grayscale"><img width="200" src="ajax-loader.gif"/><br/></span></td></tr>';
+                    $('#output tr:last').after(html);
                 }
-            })
+            });
             $.get('/mq/getMessage.php?queue_name=output_queue', null, function (data) {
                 if (data) {
-                    $("#output_queue").append(data + '<br/>');
+                    var parsed = jQuery.parseJSON(data);
+                    $("#" + parsed["task_id"] + "_thumb").html('<img src="' + parsed["thumbnail"] + '"/>');
+                    $("#" + parsed["task_id"] + "_rotated").html('<img src="' + parsed["rotated"] + '"/>');
+                    $("#" + parsed["task_id"] + "_grayscale").html('<img src="' + parsed["grayscale"] + '"/>');
                 }
             })
 
@@ -57,46 +84,48 @@ $ironmq->postMessage("input_queue", array("body" => $url_to_picture));<br/>
 
             /* get some values from elements on the page: */
             var $form = $(this),
-                    term = $form.find('input[name="url"]').val(),
-                    url = $form.attr('action');
-                    $("#pic_url").empty();
+                term = $form.find('input[name="url"]').val(),
+                url = $form.attr('action');
+            $("#pic_url").empty();
 
             /* Send the data using post and put the results in a div */
             $.post(url, { url:term },
-                    function (data) {
-                        $("#result").empty().append(data);
-                    }
+                function (data) {
+                    $("#result").empty().append(data);
+                }
             );
         });
     });
 </script>
 <style type="text/css">
-table.sample {
-	border-width: 1px;
-	border-spacing: 0px;
-	border-style: none;
-	border-color: gray;
-	border-collapse: collapse;
-	background-color: white;
-}
-table.sample th {
-	border-width: 1px;
-	padding: 1px;
-	border-style: inset;
-	border-color: gray;
-	background-color: white;
-	-moz-border-radius: ;
-}
-table.sample td {
+    table.sample {
+        border-width: 1px;
+        border-spacing: 0px;
+        border-style: none;
+        border-color: gray;
+        border-collapse: collapse;
+        background-color: white;
+    }
+
+    table.sample th {
+        border-width: 1px;
+        padding: 1px;
+        border-style: inset;
+        border-color: gray;
+        background-color: white;
+        -moz-border-radius:;
+    }
+
+    table.sample td {
         width: 300;
-	align: center;
-	border-width: 1px;
-	padding: 1px;
-	border-style: inset;
-	border-color: gray;
-	background-color: white;
-	-moz-border-radius: ;
-}
+        align: center;
+        border-width: 1px;
+        padding: 1px;
+        border-style: inset;
+        border-color: gray;
+        background-color: white;
+        -moz-border-radius:;
+    }
 </style>
 
 
--- workers/imageWorker.php
+++ workers/imageWorker.php
@@ -4,39 +4,59 @@ require_once dirname(__FILE__) . "/lib/Imgur.php";
 require_once dirname(__FILE__) . "/lib/IronMQ.class.php";
 
 $args = getArgs();
+
+print_r($args);
+
 $payload = $args['payload'];
 
 // Download image.
 $raw_image_content = file_get_contents($payload->image_url);
 $url = $payload->image_url;
-$file = substr($url,strrpos($url,'/'),strlen($url));
+$file = substr($url, strrpos($url, '/'), strlen($url));
 // Create a test source image for this example.
 $im = imagecreatefromstring($raw_image_content);
 
-$output_file = dirname(__FILE__)."/output.jpg";
+$output_file = dirname(__FILE__) . "/output";
 
 // make thumb
-$thumbWidth  = 100;
-$width = imagesx( $im );
-$height = imagesy( $im );
+$thumbWidth = 100;
+$width = imagesx($im);
+$height = imagesy($im);
 
 // calculate thumbnail size
 $new_width = $thumbWidth;
-$new_height = floor( $height * ( $thumbWidth / $width ) );
+$new_height = floor($height * ($thumbWidth / $width));
 
 // create a new temporary image
-$tmp_img = imagecreatetruecolor( $new_width, $new_height );
+$tmp_img = imagecreatetruecolor($new_width, $new_height);
 
 // copy and resize old image into new image 
-imagecopyresized( $tmp_img, $im, 0, 0, 0, 0, $new_width, $new_height, $width, $height );
-
+imagecopyresized($tmp_img, $im, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
 // save thumbnail into a file
-imagejpeg( $tmp_img, $output_file );
+imagejpeg($tmp_img, $output_file . '.jpg');
 // Upload image.
-$link = upload_file($output_file);
+$link = upload_file($output_file . '.jpg');
+
+//rotate thumbnail
+$degrees = 180;
+$rotated_thumbnail = imagerotate($tmp_img, $degrees, 0);
+// save rotated thumbnail into a file
+imagejpeg($rotated_thumbnail, $output_file . '_rotated.jpg');
+// Upload rotated image.
+$link_rotated = upload_file($output_file . '_rotated.jpg');
+
+//grayscale thumbnail
+imagefilter($tmp_img, IMG_FILTER_GRAYSCALE);
+// save grayscale thumbnail into a file
+imagejpeg($tmp_img, $output_file . '_grayscale.jpg');
+// Upload grayscale image.
+$link_grayscale = upload_file($output_file . '_grayscale.jpg');
+
+$links = array("thumbnail" => $link, "rotated" => $link_rotated, "grayscale" => $link_grayscale,"task_id" => $args['task_id']);
+
 $ironmq = new IronMQ(array(
     'token' => $payload->iron_mq->token,
     'project_id' => $payload->iron_mq->project_id
 ));
-$ironmq->postMessage("output_queue", $link);
+$ironmq->postMessage("output_queue", json_encode($links));
 ?>
